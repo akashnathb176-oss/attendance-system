5a487028f4af945774b954f180d9365c9efc5094
Akash Nath <akashnathb176@gmail.com>
2026-06-12 08:50:54 +0530

    test smtp port 2525

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ app.secret_key = 'qrattendance2026'
 DB = 'attendance.db'
 
 app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_TIMEOUT'] = 20
 mail = Mail(app)
-app.config['MAIL_TIMEOUT'] = 10
 app.config['MAIL_MAX_EMAILS'] = 5
 
 def generate_code():
